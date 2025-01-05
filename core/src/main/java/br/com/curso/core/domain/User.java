1b2928e4b7c3ffe4f6532bc388b6b509029fc01b
Gustavo <gustavooliveiracunha812@gmail.com>
2025-01-05 11:53:10 -0300

    feat: add domain classes and enums

---
+++ core/src/main/java/br/com/curso/core/domain/User.java
@@ -0,0 +1,87 @@
+package br.com.curso.core.domain;
+
+import br.com.curso.core.domain.enums.UserTypeEnum;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+
+    private String email;
+    private String password;
+    private TaxNumber taxNumber;
+    private String fullname;
+    private UserTypeEnum type;
+    private TransactionPin transactionPin;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public TaxNumber getTaxNumber() {
+        return taxNumber;
+    }
+
+    public void setTaxNumber(TaxNumber taxNumber) {
+        this.taxNumber = taxNumber;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public UserTypeEnum getType() {
+        return type;
+    }
+
+    public void setType(UserTypeEnum type) {
+        this.type = type;
+    }
+
+    public TransactionPin getTransactionPin() {
+        return transactionPin;
+    }
+
+    public void setTransactionPin(TransactionPin transactionPin) {
+        this.transactionPin = transactionPin;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ core/src/main/java/br/com/curso/core/domain/Wallet.java
@@ -0,0 +1,77 @@
+package br.com.curso.core.domain;
+
+import br.com.curso.core.domain.enums.UserTypeEnum;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Wallet {
+    private Long id;
+    private BigDecimal balance;
+    private User user;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Wallet(Long id, BigDecimal balance, User user, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.balance = balance;
+        this.user = user;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Wallet(BigDecimal balance, User user) {
+        this.balance = balance;
+        this.user = user;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Wallet() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void receiveValue(BigDecimal value){
+        this.balance.add(value);
+    }
+
+    public void transferValue(BigDecimal value){
+        if(this.user.getType() == UserTypeEnum.SHOPKEEPER){
+            throw
+        }
+        if(this.balance.compareTo(value) < 0){
+            throw
+        }
+        this.balance.subtract(value);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
